9f8fe069a8d2eab5c4084ece271f22ae2283f7ab
Artiom Manokhin <amanokhin22@ukr.net>
2022-10-16 15:21:47 +0300

    Проект окончен

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -8,12 +8,12 @@ import {useSelector} from "react-redux";
 import {useAppDispatch} from "../../redux/store";
 import {clearTodoList} from "../../redux/todoSlice";
 import {deleteTodo, fetchTodo, postTodo, putTodo} from "../../redux/asyncAction";
-import {selectTodoList} from "../../redux/selectors";
+import {selectSortedTodoList} from "../../redux/selectors";
 
 
 const Home: React.FC = () => {
 
-    const todoList = useSelector(selectTodoList);
+    const todoList = useSelector(selectSortedTodoList);
     const dispatch = useAppDispatch();
 
 
